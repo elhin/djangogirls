40bf14d27e281a02161d462cda44629611b25907
Ellyn Hindle <ellyn.hindle@gmail.com>
2025-04-16 14:48:24 -0400

    added the post repeating thing

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 # views is used to handle requests and provide a response
 # we take the request information and handle it in some way
 # then we fetch info (if needed) from database to send to html page
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
